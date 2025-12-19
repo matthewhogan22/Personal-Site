beb3c58dde75acc3175074c9a89e73486aca7864
matthewhogan22 <matthewhogan2203@gmail.com>
2025-12-19 14:41:09 -0500

    Added Navigation logic for education, projects, and work experience

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // Variable Declarations
+var next_page;
 
 // JQuery Section
 jQuery(async function($) {
@@ -9,5 +10,21 @@ jQuery(async function($) {
     var edu_button = $("#edu_box");
     edu_button.on("click", async function(e) {
         console.log("Education");
+        next_page = "Education";
+        window.location.href = "education.html";
+    });
+
+    var work_button = $("#work_box");
+    work_button.on("click", async function(e) {
+        console.log("Work Experience");
+        next_page = "Work Experience";
+        window.location.href = "work_exp.html";
+    });
+
+    var projects_button = $("#project_box");
+    projects_button.on("click", async function(e) {
+        console.log("Projects");
+        next_page = "Projects";
+        window.location.href = "projects.html";
     });
 });
